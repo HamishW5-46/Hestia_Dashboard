36a30e829636e98fde1be785baef96dd904e23dd
AfroLion <ilovetylila@gmail.com>
2025-09-07 16:51:16 -0500

    Create dashboard.php
    This is the file to be created for the index under /usr/local/hestia/web/list/dashboard/index.php

---
+++ HestiaMod/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+use function Hestiacp\quoteshellarg\quoteshellarg;
+$TAB = "DASHBOARD";
+
+// Main include
+include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
+
+// Get logged-in user
+$user = $_SESSION['user'] ?? null;
+if (!$user) {
+    die("No user logged in.");
+}
+
+// Data
+exec(HESTIA_CMD . "v-list-user-stats $user json", $output, $return_var);
+$panel[$user] = json_decode(implode("", $output), true);
+unset($output);
+
+
+
+
+// Render page
+render_page($user, $template, "list_dashboard");
+?>
